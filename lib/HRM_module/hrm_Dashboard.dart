9e7fa589b92039fb160b80d4f8c9fbabafb3d03a
Manthan <118004997+manthan0225@users.noreply.github.com>
2023-11-03 12:21:12 +0530

    TODO MANTHAN
    hrm dashboard

---
+++ lib/HRM_module/hrm_Dashboard.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HRM_Module extends StatefulWidget {
+  const HRM_Module({super.key});
+
+  @override
+  State<HRM_Module> createState() => _HRM_ModuleState();
+}
+
+class _HRM_ModuleState extends State<HRM_Module> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
