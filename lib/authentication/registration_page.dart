d366dbe8122b7d3ca5415756bb3b7eeb38932824
Manthan <118004997+manthan0225@users.noreply.github.com>
2023-11-08 12:28:08 +0530

    mansi hrms module

--- lib/authentication/registration_page.dart
+++ lib/authentication/registration_page.dart
@@ -1,44 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
-import 'package:hrmodules/authentication/registration_page.dart';
-import 'package:hrmodules/services/auth_service.dart';
-import 'package:provider/provider.dart';
+import 'package:hrmodules/HRM_module/dashBoard_View.dart';
+import 'package:hrmodules/HRM_module/hrm_Dashboard.dart';
+import 'package:hrmodules/authentication/login_page.dart';
 
-class Login_Page extends StatefulWidget {
-  const Login_Page({super.key});
+class Registration_page extends StatefulWidget {
+  const Registration_page({super.key});
 
   @override
-  State<Login_Page> createState() => _Login_PageState();
+  State<Registration_page> createState() => _Registration_pageState();
 }
 
-class _Login_PageState extends State<Login_Page> {
+class _Registration_pageState extends State<Registration_page> {
+  TextEditingController namecontroller = TextEditingController();
   TextEditingController emailController = TextEditingController();
+  TextEditingController passwordcontroller = TextEditingController();
+  TextEditingController mobilecontroller = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   bool isHidden = true;
-
-  void signin() async {
-    final authServices = Provider.of<AuthServices>(context, listen: false);
-
-    try {
-      await authServices.signInWithEmailandPassword(
-          emailController.text.toString(), passwordController.text.toString());
-    } catch (e) {
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text(e.toString())));
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Row(
         children: [
           Expanded(
-              flex: 50,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
+            flex: 50,
+            child: Image.network(
+              "https://media.istockphoto.com/id/1193287941/photo/business-colleagues-working-together-in-the-office.jpg?s=612x612&w=0&k=20&c=foSMYCMbrBehOUzBLWnyYcj3plBRGM2-rvj0UBUsVUE=",
+              fit: BoxFit.fill,
+            ),
+          ),
+          Expanded(
+            flex: 50,
+              child:Column(
                 mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Image.network(
                     "https://miro.medium.com/v2/resize:fit:740/0*-Vyqq7BQ0GYbOGZW.jpg",
@@ -46,7 +43,33 @@ class _Login_PageState extends State<Login_Page> {
                     width: Get.width * 0.2,
                   ),
                   SizedBox(
-                    height: Get.height * 0.1,
+                    height: Get.height * 0.01,
+                  ),
+                  Text("Register",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.indigo,
+                    ),
+                  ),
+                  SizedBox(
+                    height: Get.height * 0.03,
+                  ),
+                  SizedBox(
+                    width: Get.width * 0.3,
+                    child: TextField(
+                      controller: namecontroller,
+                      decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Full Name',
+                          suffixIcon: Icon(
+                            FontAwesomeIcons.person,
+                            size: 17,
+                          )),
+                    ),
+                  ),
+                  SizedBox(
+                    height: Get.height * 0.05,
                   ),
                   SizedBox(
                     width: Get.width * 0.3,
@@ -57,6 +80,22 @@ class _Login_PageState extends State<Login_Page> {
                           labelText: 'Email Address',
                           suffixIcon: Icon(
                             FontAwesomeIcons.envelope,
+                            size: 17,
+                          )),
+                    ),
+                  ),
+                  SizedBox(
+                    height: Get.height * 0.05,
+                  ),
+                  SizedBox(
+                    width: Get.width * 0.3,
+                    child: TextField(
+                      controller: mobilecontroller,
+                      decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Mobile Number',
+                          suffixIcon: Icon(
+                            FontAwesomeIcons.mobile,
                             size: 17,
                           )),
                     ),
@@ -86,27 +125,10 @@ class _Login_PageState extends State<Login_Page> {
                   SizedBox(
                     height: Get.height * 0.05,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      const Text(
-                        "Forgot Password",
-                        style: TextStyle(
-                          color: Colors.indigo,
-                          fontSize: 20,
-                        ),
-                      ),
-                      SizedBox(
-                        width: Get.width * 0.1,
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: Get.height * 0.05,
-                  ),
                   InkWell(
                     onTap: () {
-                      signin();
+                      Get.to(HRM_Module());
+
                     },
                     child: Container(
                       decoration: const BoxDecoration(
@@ -116,7 +138,7 @@ class _Login_PageState extends State<Login_Page> {
                       width: Get.width * 0.3,
                       child: const Center(
                         child: Text(
-                          "Login",
+                          "Create Account",
                           style: TextStyle(
                             color: Colors.white,
                             fontSize: 20,
@@ -129,9 +151,10 @@ class _Login_PageState extends State<Login_Page> {
                   SizedBox(
                     height: Get.height * 0.05,
                   ),
+
                   RichText(
                     text: TextSpan(
-                      text: "Don't have an have an Account ? ",
+                      text: "Alredy have an account ?  ? ",
                       style: TextStyle(
                         fontSize: 15,
                       ),
@@ -141,11 +164,11 @@ class _Login_PageState extends State<Login_Page> {
                             onTap: () {
                               Navigator.push(
                                 context,
-                                MaterialPageRoute(builder: (context) => Registration_page()),
+                                MaterialPageRoute(builder: (context) => Login_Page()),
                               );
                             },
                             child: Text(
-                              "Create an account",
+                              "Login Now",
                               style: TextStyle(
                                 color: Colors.indigo,
                                 fontSize: 15,
@@ -156,15 +179,8 @@ class _Login_PageState extends State<Login_Page> {
                       ],
                     ),
                   )
-
                 ],
-              )),
-          Expanded(
-            flex: 50,
-            child: Image.network(
-              "https://media.istockphoto.com/id/1327069166/photo/take-control-and-watch-your-company-grow.jpg?s=612x612&w=0&k=20&c=ymBoYdRZZgRfRp_pBZlTElDkRPv4bFPuqckZda-EmEw=",
-              fit: BoxFit.fill,
-            ),
+              )
           ),
         ],
       ),
--- lib/services/login_gate.dart
+++ lib/services/login_gate.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hrmodules/HRM_module/hrm_Dashboard.dart';
 import 'package:hrmodules/authentication/login_page.dart';
 
